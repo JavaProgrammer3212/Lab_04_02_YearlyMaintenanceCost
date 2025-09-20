6b131b70980284fb6c815c7cf9752b84d6a413db
JavaProgrammer3212 <wheatlan@mail.uc.edu>
2025-09-20 11:53:33 -0400

    Final Commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int springCosts = 415;
+
+        int summerCosts = 212;
+
+        int fallCosts = 450;
+
+        int winterCosts = 1200;
+
+        int totalCosts;
+
+        System.out.println("The maintenance costs for the Spring season were $" + springCosts);
+
+        System.out.println("The maintenance costs for the Summer season were $" + summerCosts);
+
+        System.out.println("The maintenance costs for the Fall season were $" + fallCosts);
+
+        System.out.println("The maintenance costs for the Winter season were $" + winterCosts);
+
+        totalCosts = springCosts + summerCosts + fallCosts + winterCosts;
+
+        System.out.println("The maintenance costs for the whole year was $" + totalCosts);
+
+    }
+}
